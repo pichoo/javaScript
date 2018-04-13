269fbd8a8fbd0de07b1dae5b45800a4a2de7d25d
umarmuha <muhammad_umar@dell.com>
2018-04-12 23:31:52 -0500

    added append option if the name exists in the dictionary

--- phonebook.py
+++ phonebook.py
@@ -24,23 +24,35 @@ phonebook_menu()
 
 while menu_choice != 5:
     menu_choice = int(input("Select a menu Option (1-5): "))
+
+        
     
 
 #When choosing option 1 it should accept a first name, last name and a phone number
 
     if menu_choice == 1:
         print("Add a new Phone Book Entry:")
+        
         first_name = str(input("First Name: "))
-        if not first_name.isalpha:
-            phonebook_menu()
+        if not first_name.isalpha():
+            print("\nYou have entered a wrong first name. Please try again with letters only with no spaces and special characters")
+            continue
             
         last_name  = str(input("Last Name: "))
-        if not last_name.isalpha:
-            phonebook_menu()
-            
-        name       = first_name + "_" +last_name
-        phone      = int(input("Phone Number: "))
-        email      = str(input("Email Address: "))  
+        if not last_name.isalpha():
+            print("\nYou have entered a wrong last name. Please try again with letters only with no spaces and special characters")
+            continue
+        
+        phone = input("Phone Number: ")
+        if not phone.isdigit():
+            print("\nYou have entered a wrong Phone Number. Please try again with numbers only")
+            continue
+        
+        name = first_name + "_" +last_name
+        if name in contact_info:
+            contact_info[name]['phone_number'].append(phone)
+
+        email = str(input("Email Address: "))  
         if isValidEmail(email) == True:
             
             tempdict = {}
@@ -51,22 +63,18 @@ while menu_choice != 5:
             
             contact_info[name] = tempdict
 
-            #[phone,email] #nested hash format should be contact_info=['name': {'Phone Number':phone, 'Email address':email }]
+#[phone,email] #nested hash format should be contact_info=['name': {'Phone Number':phone, 'Email address':email }]
        
         else:
             email = input("Please enter a valid email id: ")
             continue
-            
- #       if email != re.match(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email):
- #           email = input("Please try again with a valid email address: ")
- #       else:
- #      continue
+
         
     elif menu_choice == 2:
         print("\nType the Full Name to remove from the Phone Book")
         name = input ("Name: ")
         if name in contact_info:
-            del contact_info[name]
+            del contact_info[name]['phone_number']
         else:
             print("Name was not found")
     
@@ -74,7 +82,6 @@ while menu_choice != 5:
         print("\nLookup Contact Information")
         name = input("\nName: ")
         if name in contact_info:
-            #print(f("Full Name:{name}")
             print(contact_info)
             #print(contact_info[name][phone_number])
             #print(contact_info[name][email_address]) #, "\nPhone: " + contact_info[name]['phone_number'], "\nEmail: " + contact_info[name]['email_address'])
@@ -83,5 +90,4 @@ while menu_choice != 5:
 
     elif menu_choice != 5:
         phonebook_menu()
-    
     
